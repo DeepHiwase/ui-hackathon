f6a607575d079ea7c7ed112d1ae4c35a9c186f6d
Rahulsonale <rahulsonale002@gmail.com>
2026-02-25 13:57:35 +0530

    bg img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,24 @@
+import img from "../assets/landing-page-coofee-machine.png";
+
 export const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <main className="flex-1 p-8 bg-[#ECE7D1] min-h-[calc(100vh-60px)] ">
+        <div className="max-w-[500px]">
+          <h1 className="text-5xl font-bold mb-4 mt-10 font-display">
+            “Because every great day begins with great coffee.” ☕
+          </h1>
+
+          <p className="text-gray-600 max-w-xl font-Georgia text- xl font-serif">
+            Discover machines that turn simple mornings into unforgettable
+            rituals.Precision engineering meets timeless design — made for true
+            coffee enthusiasts.
+          </p>
+        </div>
+        <div>
+          <img src={img} alt="" />
+        </div>
+      </main>
+    </>
+  );
 };
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -4,6 +4,7 @@ import { HiMenu, HiX } from "react-icons/hi";
 import { MdOutlineFeaturedPlayList } from "react-icons/md";
 import { GrGallery } from "react-icons/gr";
 import { IoIosCall } from "react-icons/io";
+import { Hero } from "../components/Hero";
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -42,37 +43,34 @@ export const Header = () => {
           </div>
         )}
       </nav>
-      <aside
-        className="group fixed top-[60px] left-0 
-             h-[calc(100vh-60px)] 
-             w-[50px] hover:w-[250px] 
-             bg-[#C9B59C] shadow-md 
-             transition-all duration-300 ease-in-out"
-      >
-        <div className="flex flex-col gap-8 p-4 font-serif text-lg">
-          <div className="flex items-center gap-4 cursor-pointer hover:text-[#6E5034]">
-            <MdOutlineFeaturedPlayList size={24} />
-            <span className="hidden group-hover:inline transition-all duration-300">
-              Features
-            </span>
-          </div>
+      <div className="flex">
+        <aside
+          className="group 
+               h-[calc(100vh-60px)] 
+               w-[60px] hover:w-[250px] 
+               bg-[#C9B59C] shadow-md 
+               transition-all duration-300 ease-in-out"
+        >
+          <div className="flex flex-col gap-8 p-4 font-serif text-lg">
+            <div className="flex items-center gap-4 cursor-pointer hover:text-[#6E5034]">
+              <MdOutlineFeaturedPlayList size={24} />
+              <span className="hidden group-hover:inline">Features</span>
+            </div>
 
-          <div className="flex items-center gap-4 cursor-pointer hover:text-[#6E5034]">
-            <GrGallery size={24} />
-            <span className="hidden group-hover:inline transition-all duration-300">
-              Gallery
-            </span>
-          </div>
+            <div className="flex items-center gap-4 cursor-pointer hover:text-[#6E5034]">
+              <GrGallery size={24} />
+              <span className="hidden group-hover:inline">Gallery</span>
+            </div>
 
-          <div className="flex items-center gap-4 cursor-pointer hover:text-[#6E5034]">
-            <IoIosCall size={24} />
-            <span className="hidden group-hover:inline transition-all duration-300">
-              Call To Action
-            </span>
+            <div className="flex items-center gap-4 cursor-pointer hover:text-[#6E5034]">
+              <IoIosCall size={24} />
+              <span className="hidden group-hover:inline">Call To Action</span>
+            </div>
           </div>
-        </div>
-      </aside>
-      <div className="bg-black"></div>
+        </aside>
+
+        <Hero />
+      </div>
     </>
   );
 };
